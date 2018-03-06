3437f996917e4b2575ff1bc4b843ff9d7c469c2e
George Alatrash <george.alatrash@aurea.com>
2018-03-06 11:02:00 -0800

    Fixing release version and notes.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SecurityAnalyzer")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 - 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("8.1.2.0")]
-[assembly: AssemblyFileVersion("8.1.2.0")]
+[assembly: AssemblyVersion("8.1.5.0")]
+[assembly: AssemblyFileVersion("8.1.5.0")]
